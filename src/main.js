05cd4cdfb42ac4081fa7e271038097504be9c5d2
MannyFluss <manny.fluss@gmail.com>
2022-05-05 14:31:58 -0700

    typescript def

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-// Distracction
-// Manny Fluss, Benjamin Paulsen, and Manas Sara
-// Date Completed: 5/1/22
-
-// Creative Tilt:
-// Technical: We are super proud of the system we came up with for texting the raccoons. We created a great looking phone messaging app, which gives the player a randomly generated prompt to copy and highlights the letters as they type. When the player enters the prompt correctly, it is sent as a message and the raccoons will give a randomized reply. This was definitely beyond the examples shown in class and makes our game's message about distracted texting really come through.
-// Visual: We put a lot of effort into the game's sprites, animations, and music to make it cute and visually appealing. In particular, we wanted the assets to live up to the goofy premise of an army of raccoons with a texting addiction. We think we did something clever by combining the movement and dodging of an endless runner with the experience of texting-while-walking.
+/**@type {import("../typings/phaser")} */
 
 let canvasStyle = " \
 border-radius: 10px; \
@@ -12,8 +6,8 @@ border-radius: 10px; \
 
 let config = {
     type: Phaser.CANVAS,
-    width: 179*6, // street.png width * export resize factor
-    height: 480,
+    width: 768, // street.png width * export resize factor
+    height: 432,
     scene : [],
     backgroundColor : "#1982FC",
     canvasStyle: canvasStyle
