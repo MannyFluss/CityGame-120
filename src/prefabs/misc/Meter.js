71333d5c0cc4ec0830dc2d5eca9b11c532372c36
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2022-05-29 14:36:42 -0700

    add tween to progress bar changes

--- src/prefabs/misc/Meter.js
+++ src/prefabs/misc/Meter.js
@@ -2,13 +2,25 @@ class Meter extends Phaser.GameObjects.TileSprite {
     constructor(scene, x, y, width, height, texture, frame) {
         super(scene, x, y, width, height, texture, frame);
         scene.add.existing(this);
+        this.scene = scene;
 
         this.total = width;
+        this.width = 1;
     }
 
     // set the meter from 0 to 1
     set(value) {
-        // Math.max prevents zero width error
-        this.width = Math.max(value * this.total, 1);
+        if (this.width != this.total) 
+        {
+            // Math.max prevents zero width error
+            let newWidth = Math.max(value * this.total, 1);
+    
+            this.scene.tweens.add({
+                targets: this,
+                width: newWidth,
+                duration: 200,
+                ease: 'Power2',
+            });
+        }
     }
 }
